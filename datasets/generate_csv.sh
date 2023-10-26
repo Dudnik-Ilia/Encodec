51341d9bfd95acb7c83fcaec95370e746ac093cf
Dudnik-Ilia <dudnik.ilja@rambler.ru>
2023-10-26 16:43:55 +0200

    Set the paths in configs and HPC_task

--- datasets/generate_csv.sh
+++ datasets/generate_csv.sh
@@ -3,12 +3,12 @@
 TRAIN_WITHOUT_EXT=$(basename "$TRAIN_FILE" .tar.gz)
 TEST_WITHOUT_EXT=$(basename "$TEST_FILE" .tar.gz)
 
-input_file='$TMPDIR/$SLURM_JOBID/datasets/data/LibriSpeech/$TRAIN_WITHOUT_EXT'
-output_file='$TMPDIR/$SLURM_JOBID/datasets/librispeech_dataset_train.csv'
+input_file="$TMPDIR/$SLURM_JOBID/datasets/data/LibriSpeech/$TRAIN_WITHOUT_EXT"
+output_file="$TMPDIR/$SLURM_JOBID/datasets/librispeech_dataset_train.csv"
 python $HOME/Encodec/datasets/generate_desc_file.py -i $input_file -o $output_file
 echo "Generated TRAIN csv file"
 
-input_file='$TMPDIR/$SLURM_JOBID/datasets/data/LibriSpeech/$TEST_WITHOUT_EXT'
-output_file='$TMPDIR/$SLURM_JOBID/datasets/librispeech_dataset_test.csv'
+input_file="$TMPDIR/$SLURM_JOBID/datasets/data/LibriSpeech/$TEST_WITHOUT_EXT"
+output_file="$TMPDIR/$SLURM_JOBID/datasets/librispeech_dataset_test.csv"
 python $HOME/Encodec/datasets/generate_desc_file.py -i $input_file -o $output_file
 echo "Generated TEST csv file"
