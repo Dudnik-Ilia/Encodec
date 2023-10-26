39e8166a0cae106e9cca90af948883a615c653bf
Dudnik-Ilia <dudnik.ilja@rambler.ru>
2023-10-26 16:14:52 +0200

    Set the paths in configs and HPC_task

--- datasets/generate_csv.sh
+++ datasets/generate_csv.sh
@@ -5,10 +5,10 @@ TEST_WITHOUT_EXT="${TEST_FILE%.*}"
 
 input_file='$TMPDIR/$SLURM_JOBID/datasets/data/LibriSpeech/$TRAIN_WITHOUT_EXT'
 output_file='$TMPDIR/$SLURM_JOBID/datasets/librispeech_dataset_train.csv'
-python generate_desc_file.py -i $input_file -o $output_file
+python $HOME/Encodec/datasets/generate_desc_file.py -i $input_file -o $output_file
 echo "Generated TRAIN csv file"
 
 input_file='$TMPDIR/$SLURM_JOBID/datasets/data/LibriSpeech/$TEST_WITHOUT_EXT'
 output_file='$TMPDIR/$SLURM_JOBID/datasets/librispeech_dataset_test.csv'
-python generate_desc_file.py -i $input_file -o $output_file
+python $HOME/Encodec/datasets/generate_desc_file.py -i $input_file -o $output_file
 echo "Generated TEST csv file"
